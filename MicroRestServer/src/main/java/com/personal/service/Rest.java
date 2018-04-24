07cb75635502926cd06a98a90d9d3692d835488a
LHerreria <LHerreria@HIQTW420.ad.harman.com>
2018-04-24 16:58:19 -0500

    First commit

---
+++ MicroRestServer/src/main/java/com/personal/service/Rest.java
@@ -0,0 +1,17 @@
+package com.personal.service;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/rest")
+public class Rest {
+	
+	@POST
+	@Path("/isServerUp")
+	@Produces({MediaType.APPLICATION_JSON})
+	public boolean isServerUp() {
+		return true;
+	}
+}
---
+++ MicroRestServer/src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Title of the document</title>
+</head>
+
+<body>
+	<h1>Welcome user</h1>
+	<p>Check if server is up <a href="">here</a></p>
+</body>
+
+</html>
